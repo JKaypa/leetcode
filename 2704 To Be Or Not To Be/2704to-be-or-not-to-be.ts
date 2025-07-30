f907f905d3e0af53f7fa76a0ae1564458fa3a157
JKaypa <jose_kaypa@hotmail.com>
2025-07-30 14:50:11 +0000

    Code challenge submission for question: 2704 To Be Or Not To Be

---
+++ 2704 To Be Or Not To Be/2704to-be-or-not-to-be.ts
@@ -0,0 +1,32 @@
+type ToBeOrNotToBe = {
+    toBe: (val: any) => boolean;
+    notToBe: (val: any) => boolean;
+};
+
+function expect(val: any): ToBeOrNotToBe {
+    const toBe = (value) => {
+        if(val === value){
+            return true
+        } else {
+            throw new Error('Not Equal')
+        }
+    }
+
+    const notToBe = (value) => {
+        if(val !== value){
+            return true
+        } else {
+            throw new Error('Equal')
+        }
+    }
+
+    return {
+        toBe,
+        notToBe
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
