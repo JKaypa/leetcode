cfcafe71e8af79c8e8cbc95a4324cb4a74f333e3
JKaypa <jose_kaypa@hotmail.com>
2025-08-04 17:30:50 +0000

    Code challenge submission for question: 905 Sort Array By Parity

---
+++ 905 Sort Array By Parity/905sort-array-by-parity.ts
@@ -0,0 +1,15 @@
+function sortArrayByParity(nums: number[]): number[] {
+    let left = 0;
+    
+    for(let right = 0; right < nums.length; right++){
+        if(nums[right] % 2 === 0){
+            const temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp
+            
+            left++
+        }
+    }
+    
+    return nums;
+};
