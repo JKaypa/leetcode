f178f6a7ee47285d29a452ab9261c1962b0d6e6b
JKaypa <jose_kaypa@hotmail.com>
2025-08-04 19:24:28 +0000

    Code challenge submission for question: 1299 Replace Elements with Greatest Element on Right Side

---
+++ 1299 Replace Elements with Greatest Element on Right Side/1299replace-elements-with-greatest-element-on-right-side.ts
@@ -0,0 +1,12 @@
+function replaceElements(arr: number[]): number[] {
+    let max = -1
+    
+    for(let i = arr.length - 1; i >= 0; i--) {
+        let temp = arr[i];
+        arr[i] = max;
+        
+        max = Math.max(temp, max)
+    }
+    
+    return arr;
+};
