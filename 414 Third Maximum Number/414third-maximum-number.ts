cc87c3b4d8d9e61d966d04c4e04544bccf808572
JKaypa <jose_kaypa@hotmail.com>
2025-08-20 21:59:33 +0000

    Code challenge submission for question: 414 Third Maximum Number

---
+++ 414 Third Maximum Number/414third-maximum-number.ts
@@ -0,0 +1,31 @@
+function thirdMax(nums: number[]): number {
+    if(!nums.length){
+        return 0
+    }
+    
+    if(nums.length < 3){
+        return Math.max(...nums)
+    }
+    
+    let max = -Infinity
+    let secondMax = max
+    let thirdMax = secondMax
+    
+    for(const num of nums){
+        if(num > max) {
+            thirdMax = secondMax
+            secondMax = max
+            max = num
+        } else if(num < max && num > secondMax){
+            thirdMax = secondMax
+            secondMax = num
+        } else if(num < secondMax && num > thirdMax){
+            thirdMax = num
+        }
+    }
+    
+    const isThirdMaxExist = thirdMax !== -Infinity
+    
+    return isThirdMaxExist ? thirdMax : max
+    
+};
