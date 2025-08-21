4b7e0270acd8594e9a87a181c76676e4b0a258b8
JKaypa <jose_kaypa@hotmail.com>
2025-08-21 21:56:45 +0000

    Code challenge submission for question: 448 Find All Numbers Disappeared in an Array

--- 448 Find All Numbers Disappeared in an Array/448find-all-numbers-disappeared-in-an-array.js
+++ 448 Find All Numbers Disappeared in an Array/448find-all-numbers-disappeared-in-an-array.js
@@ -6,16 +6,13 @@ var findDisappearedNumbers = function(nums) {
     // a better optimization Time Big O(n), Space Big O(n)
     const n = nums.length
     const numsMap = new Map()
-    const allNumsArr = []
     const output = []
     
     for(const num of nums){
         numsMap.set(num, num)
     }
     
-    for(let i = 1; i <= n; i++){
-        allNumsArr.push(i)
-    }
+    const allNumsArr = Array.from({length: n}, (_, i) => ++i)
     
     for(const num of allNumsArr) {
         if(!numsMap.has(num)){
